19cdb864394105e07e59c82ac545311351a45f85
PrinceJamesss <Birdiee09@gmail.com>
2021-09-02 16:15:02 -0400

    Initial Commit
    Bot Framework Complete

---
+++ main.js
@@ -0,0 +1,21 @@
+//Call Discord
+const Discord = require('discord.js');
+//Call DotENV
+require('dotenv').config();
+
+//Required in Discord.js V13, Intents
+const client = new Discord.Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"]
+})
+
+//Command and Event Handlers
+client.commands = new Discord.Collection();
+client.commands = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+// Discord Login
+client.login(process.env.DISCORD_TOKEN);
